33ec0ccf55b3b975117253f55ee7963e9c483064
unknown <eric.turriff@gmail.com>
2018-04-27 20:12:08 -0400

    defaulting sudoku to easy

--- src/difficulty.jsx
+++ src/difficulty.jsx
@@ -1,11 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-import { withRouter, BrowserRouter, Route } from "react-router-dom";
-import Sudoku from "t-sudoku-generator";
+import { withRouter, BrowserRouter, Route } from 'react-router-dom';
+import Sudoku from 't-sudoku-generator';
 
 class Difficulty extends React.Component {
-  state = { difficulty: "easy" };
+  state = { difficulty: 'easy' };
+  componentDidMount() {
+    this.props.history.push('/easy');
+  }
+
   onDifficultyHandler = e => {
+    if (this.props.location.pathname.split('/').pop() === e.target.value) {
+      return;
+    }
     this.props.history.push(`/${e.target.value}`);
     this.setState({
       difficulty: e.target.value
@@ -16,12 +23,12 @@ class Difficulty extends React.Component {
       <Fragment>
         <select
           onChange={this.onDifficultyHandler}
-          style={{ marginBottom: "10px" }}
+          style={{ marginBottom: '10px' }}
           value={this.state.difficulty}
         >
-          <option value="easy">{"easy"}</option>
-          <option value="medium">{"medium"}</option>
-          <option value="hard">{"hard"}</option>
+          <option value="easy">{'easy'}</option>
+          <option value="medium">{'medium'}</option>
+          <option value="hard">{'hard'}</option>
         </select>
       </Fragment>
     );
