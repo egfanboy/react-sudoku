156bbc1b4c63faa86694407414d239c8f191bb75
Rudney Netto <rudneynetto@gmail.com>
2018-10-07 03:21:42 -0300

    improvement: add storybook background addon

--- src/button/button.story.js
+++ src/button/button.story.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import backgrounds from '../../.storybook/backgrounds';
 import ButtonBar from './button-bar';
 
-storiesOf('Button Bar', module).add('default', () => (
-  <ButtonBar enabledButtons={[]} />
-));
+storiesOf('Button Bar', module)
+  .addDecorator(backgrounds)
+  .add('default', () => <ButtonBar enabledButtons={[]} />);
--- src/dialog/dialog.story.js
+++ src/dialog/dialog.story.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import backgrounds from '../../.storybook/backgrounds';
 import Dialog from './dialog';
 
-storiesOf('Dialog', module).add('simple dialog', () => (
-  <Dialog
-    message="Hi, this is a message"
-    completionTimeMessage="Finished in 10 minutes!"
-    isOpen
-  />
-));
+storiesOf('Dialog', module)
+  .addDecorator(backgrounds)
+  .add('simple dialog', () => (
+    <Dialog
+      message="Hi, this is a message"
+      completionTimeMessage="Finished in 10 minutes!"
+      isOpen
+    />
+  ));
