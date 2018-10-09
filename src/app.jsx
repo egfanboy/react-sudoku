2552da55e026368aa06be4df2a95f371886a0339
Kartik Pandey <kartikpandey2@gmail.com>
2018-10-10 01:52:58 +0530

    fix: implementation error

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,9 @@
 import React, { Fragment, Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { easy, medium, hard } from 't-sudoku-generator';
+import { Button } from 'antd';
 import { Screen } from './splash-screen';
-
 import StyledApp from './app.styled';
 import { Board } from './game';
 
-const Easy = () => <Board board={easy()} />;
-const Medium = () => <Board board={medium()} />;
-const Hard = () => <Board board={hard()} />;
 
 class App extends Component {
   state = {
@@ -19,21 +14,22 @@ class App extends Component {
     this.setState({ username: name });
   };
 
+  handleClick = () => {
+    this.setState({ username: null }, () => sessionStorage.clear());
+  }
+
   render() {
     const { username } = this.state;
     return (
       <StyledApp>
-        <BrowserRouter>
-          {!username ? (
-            <Screen notify={name => this.changeScreen(name)} />
-          ) : (
-            <Fragment>
-              <Route exact path="/easy" component={Easy} />
-              <Route exact path="/medium" component={Medium} />
-              <Route exact path="/hard" component={Hard} />
-            </Fragment>
-          )}
-        </BrowserRouter>
+        {!username ? (
+          <Screen notify={name => this.changeScreen(name)} />
+        ) : (
+          <Fragment>
+            <Board />
+            <Button size="large" onClick={() => this.handleClick()}>Change level</Button>
+          </Fragment>
+        )}
       </StyledApp>
     );
   }
--- src/difficulty/difficulty.jsx
+++ src/difficulty/difficulty.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 import { Select } from 'antd';
+import { _events } from '../game/sudoku';
 
 const { Option } = Select;
 
