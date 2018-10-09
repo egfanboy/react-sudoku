86b91180cd45fcc74f4b76cd8f646774b0675b78
bagshik <bagshik@gmail.com>
2018-10-09 21:57:06 +0200

    Move ThemeProvider to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,33 @@
 import React, { Fragment } from 'react';
-
+import { ThemeProvider } from 'styled-components';
 import StyledApp from './app.styled';
 import { Difficulty } from './difficulty';
 import { Board } from './game';
+import { getTheme } from './themes';
 
-const App = () => (
-  <StyledApp>
-    <Fragment>
-      <Difficulty />
-      <Board />
-    </Fragment>
-  </StyledApp>
-);
+class App extends React.Component {
+  state = {
+    theme: getTheme(),
+  };
+
+  handleChangeTheme = name => {
+    this.setState({ theme: getTheme(name) });
+  };
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledApp>
+          <Fragment>
+            <Difficulty />
+            <Board changeTheme={this.handleChangeTheme} />
+          </Fragment>
+        </StyledApp>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default App;
