a44cd228e7f21f270c2caae15a0b4b9dfdf9a91d
Tudor Pavel <tudorv.pavel@gmail.com>
2018-10-12 00:08:48 +0300

    add PropTypes to Sudoku component
    also fixes bug related to onComplete being an object because of using storybook plural `actions` instead of singular `action`

--- .storybook/stories/game.story.jsx
+++ .storybook/stories/game.story.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { actions } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 
 import App from '../../src/app';
 
@@ -13,7 +13,7 @@ class Game extends React.Component {
   render() {
     return (
       <div>
-        <App difficulty={this.state.difficulty} onComplete={actions('done')} />
+        <App difficulty={this.state.difficulty} onComplete={action('done')} />
         <button onClick={() => this.setState({ difficulty: 'easy' })}>
           Easy
         </button>
