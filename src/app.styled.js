360d1136009c0180ea946da80c22c2cd94c80e7d
ericturf <eric.turriff@gmail.com>
2019-01-22 08:55:12 -0500

    Removed useless wrapper styles

--- src/app.styled.js
+++ src/app.styled.js
@@ -2,12 +2,9 @@ import styled from 'styled-components';
 
 const StyledApp = styled.div`
   display: flex;
-  background-color: white;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  background-color: ${theme => theme.background};
 `;
 
 export default StyledApp;
